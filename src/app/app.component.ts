28f95b06b2431a6ec77db677683f1d519b088db0
clintkingston <ckingston1@woolworths.com.au>
2024-04-28 14:00:20 +1000

    added responsive navigation menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./modules/shared/header/header.component";
+import { NavigationComponent } from "./modules/shared/navigation/navigation.component";
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent, MatCardModule],
 })
-export class AppComponent {
-}
+export class AppComponent {}
---
+++ src/app/modules/shared/navigation/navigation.component.ts
@@ -0,0 +1,53 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import {
+  state,
+  style,
+  animate,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css',
+  animations: [
+    trigger('expandCollapse', [
+      state(
+        'collapsed',
+        style({
+          height: '0',
+          overflow: 'hidden',
+          opacity: '0',
+        })
+      ),
+      state(
+        'expanded',
+        style({
+          height: '*',
+          overflow: 'hidden',
+          opacity: '1',
+        })
+      ),
+      transition('collapsed => expanded', [animate('0.25s ease-out')]),
+      // Define the reverse transition
+      transition('expanded => collapsed', [animate('0.25s ease-out')]),
+    ]),
+  ],
+})
+export class NavigationComponent {
+  navOptions = ['about', 'blog', 'portfolio', 'contact'];
+  isMenuVisible: boolean = true;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    // Adjust the menu visibility based on screen size
+    this.isMenuVisible = window.innerWidth > 430;
+  }
+  toggleMenu(): void {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+}
